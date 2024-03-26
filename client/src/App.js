33b1bfd622dc19d376249904c6b047b98c5b10d1
Guilherme Filheiro <guilhermefilheiro@gmail.com>
2024-03-26 17:13:40 -0300

    Configured Routes with navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './pages/Login';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>Dashboard</div>} />
           <Route path='/login/' element={<Login/>} />
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<Home/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <section className="navbar">
+            <h2>Header</h2>
+        </section>
+    );
+}
